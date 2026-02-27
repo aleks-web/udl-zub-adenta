12da19d314860b8242318d205459e09a190a3c8d
Alex <dok.go@yandex.ru>
2026-02-27 15:32:52 +0500

    fc

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,4 @@
 import { defineConfig } from "vite";
-import includeHtml from "vite-plugin-include-html";
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default defineConfig({
-    base: './',
-    plugins: [includeHtml()],
-    resolve: {
-        alias: {
-            '@img': resolve(__dirname, './src/assets/images')
-        }
-    },
+    base: './'
 });
